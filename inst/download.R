68ebe9ad422e02019298f43109879fd54daa7b90
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-09-19 10:06:00 -0400

    update name and version

--- inst/download.R
+++ inst/download.R
@@ -1,22 +1,27 @@
-if(!file.exists("../data/ESL.mixture.RData")){
-  download.file(
-    "https://web.stanford.edu/~hastie/ElemStatLearn/datasets/ESL.mixture.rda",
-    "../data/ESL.mixture.RData")
+library(data.table)
+data.url.vec <- c(
+  "https://web.stanford.edu/~hastie/ElemStatLearn/datasets/prostate.data",
+  "https://web.stanford.edu/~hastie/ElemStatLearn/datasets/ESL.mixture.rda",
+  "http://github.com/tdhock/animint-examples/blob/master/data/PSJ.RData?raw=true")
+for(data.url in data.url.vec){
+  parsed <- nc::capture_first_vec(
+    data.url, ".*/", name=".*", "[.]", suffix="[^?]+")
+  print(data.url)
+  print(parsed)
+  dest.RData <- sprintf("../data/%s.RData", parsed$name)
+  if(!file.exists(dest.RData)){
+    data_is_csv <- parsed$suffix=="data"
+    download.file(data.url, dest.RData, mode=if(data_is_csv)"w" else "wb")
+    objs <- if(data_is_csv){
+      assign(parsed$name, fread(dest.RData))
+      parsed$name
+    }else{
+      load(dest.RData)
+    }
+    save(list=objs, file=dest.RData, compress="xz")
+  }
+  dest.Rd <- sprintf("../man/%s.Rd", parsed$name)
+  if(!file.exists(dest.Rd)){
+    prompt(name=objs, file=dest.Rd)
+  }
 }
-(objs=load("../data/ESL.mixture.RData"))
-
-if(!file.exists("../data/prostate.RData")){
-  download.file(
-    "https://web.stanford.edu/~hastie/ElemStatLearn/datasets/prostate.data",
-    "prostate.data")
-}
-prostate <- data.table::fread("prostate.data")
-save(prostate, file="../data/prostate.RData")
-
-if(!file.exists("../data/PSJ.RData")){
-  u <- paste0(
-    "http://github.com/tdhock/animint-examples",
-    "/blob/master/data/PSJ.RData?raw=true")
-  download.file(u, "../data/PSJ.RData", mode="wb")
-}
-(objs=load("../data/PSJ.RData"))
